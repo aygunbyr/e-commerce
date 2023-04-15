52f9cc09b7a2e4ad3500e9d9c35a9d1665822e23
Aygün Bayır <turkoaygun@gmail.com>
2023-04-15 13:08:43 +0300

    Update products page: implemented useInfiniteQuery

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,20 +1,59 @@
-import { Grid } from "@chakra-ui/react";
-import { useQuery } from "react-query";
+import React from "react";
+import { Box, Button, Flex, Grid } from "@chakra-ui/react";
+import { useInfiniteQuery } from "react-query";
 import Card from "../../components/Card";
 import { fetchProductList } from "../../api";
 
 function Products() {
-  const { isLoading, error, data } = useQuery("products", fetchProductList);
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery("products", fetchProductList, {
+    getNextPageParam: (lastGroup, allGroups) => {
+      const morePagesExist = lastGroup?.length === 12;
+      if (!morePagesExist) {
+        return;
+      }
 
-  if (isLoading) return "Loading...";
+      return allGroups.length + 1;
+    },
+  });
 
-  if (error) return "An error has occurred: " + error.message;
+  if (status === "loading") return "Loading...";
+
+  if (status === "error") return "An error has occurred: " + error.message;
 
   return (
     <div>
       <Grid templateColumns="repeat(3, 1fr)" gap={4}>
-        {data && data.map((item, key) => <Card key={key} item={item} />)}
+        {data.pages.map((group, i) => (
+          <React.Fragment key={i}>
+            {group.map((item) => (
+              <Box w="100%" key={item._id}>
+                <Card item={item}></Card>
+              </Box>
+            ))}
+          </React.Fragment>
+        ))}
       </Grid>
+      <Flex mt="10" mb="10" justifyContent="center">
+        <Button
+          colorScheme={hasNextPage ? "pink" : null}
+          isLoading={isFetchingNextPage}
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}
+        >
+          {isFetchingNextPage
+            ? "Loading more..."
+            : hasNextPage
+            ? "Load More"
+            : "Nothing more to load"}
+        </Button>
+      </Flex>
     </div>
   );
 }
